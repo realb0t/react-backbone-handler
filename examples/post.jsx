973f4ac3e8e513b8512fed5c144ea4905f13f269
nkazantsev <kazantsev.nickolay@gmail.com>
2015-07-07 11:13:31 +0300

    Validate and refactoring realization and update example

--- examples/post.jsx
+++ examples/post.jsx
@@ -1,11 +1,11 @@
 var Post = React.createClass({
-  mixins: [ 'ModelHandlerMixin' ],
+  mixins: [ ReactBackboneHandlerMixin ],
   propTypes: {
     post: React.PropTypes.instanceOf(Post).isRequired
   },
   getInitialState: function() {
     return _(this.getInitializeModelState(this.props)).extend({
-      other: null
+      label: null
     });
   },
   handleModels: {
@@ -16,12 +16,21 @@ var Post = React.createClass({
     }
   },
   autobinding: true,
+  handleChangeDescription: function(e) {
+    var desc = $(e.target).val();
+    this.setState({ label: desc });
+    return this.props.post.set({ desc: desc });
+  },
   render: function() {
     return (
       <div>
         <input name="title" 
           value={this.state.title} 
           onChange={this.handleChangeEvent.bind(this, 'post', 'title')} />
+        <input name="desc" 
+          value={this.state.post_description} 
+          onChange={this.handleChangeDescription} />
+        <label>{this.state.label}</label>
       </div>
     );
   }
